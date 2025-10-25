529bfe4032cc156eb75214fa264e709d4479c9b7
devi63 <devanshifaldu@gmail.com>
2025-10-25 15:20:22 +0530

    editing

--- app.py
+++ app.py
@@ -3,9 +3,24 @@ import pickle
 import pandas as pd
 import os
 # from dotenv import load_dotenv
+import requests
+
+
+
+
 
 api_key = os.getenv('TMDB_API_KEY')
 access_token = os.getenv('TMDB_ACCESS_TOKEN')
+
+def fetch_poster(movie_id):
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US"
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
 
 
 def recommend(movie):
@@ -14,11 +29,13 @@ def recommend(movie):
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
 
     recommended_movies = []
+    recommended_movie_posters = []
     for i in movies_list:
         movie_id=i[0]    # for movie id for pposter
-        #feth poster from api 
         recommended_movies.append(movies.iloc[i[0]].title)
-    return recommended_movies
+        #feth poster from api 
+        recommended_movie_posters.append(fetch_poster(movie_id))
+    return recommended_movies,recommended_movie_posters
 
 
 # Load data
@@ -38,5 +55,19 @@ if st.button("Recommend"):
         st.write(i)
   
 
-
-# we will fecth poster usin id
+col1, col2, col3, col4, col5 = st.columns(5)
+with col1:
+    st.text(recommendations[0][0])
+    st.image(recommendations[1][0])
+with col2:
+    st.text(recommendations[0][1])
+    st.image(recommendations[1][1])
+with col3:
+    st.text(recommendations[0][2])
+    st.image(recommendations[1][2])
+with col4:
+    st.text(recommendations[0][3])
+    st.image(recommendations[1][3])
+with col5:
+    st.text(recommendations[0][4])
+    st.image(recommendations[1][4])
