8ee1d6eda0846486e6382c3d54f664c45f9e3176
devi63 <devanshifaldu@gmail.com>
2025-10-25 15:57:22 +0530

    semi_final version

--- app.py
+++ app.py
@@ -8,19 +8,37 @@ import requests
 
 
 
+api_key = "c7633645da556bf52bb3c968dbd6d375"   
 
-api_key = os.getenv('TMDB_API_KEY')
-access_token = os.getenv('TMDB_ACCESS_TOKEN')
+#api_key = os.getenv('TMDB_API_KEY')
+#access_token = os.getenv('TMDB_ACCESS_TOKEN')
+
+#def fetch_poster(movie_id):
+    #url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US"
+    #response = requests.get(url)
+    #data = response.json()
+    #print(data)
+    #poster_path = data['poster_path']
+    #full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    #return full_path
 
 def fetch_poster(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US"
     response = requests.get(url)
     data = response.json()
-    print(data)
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    print("Response keys:", data.keys())
 
+
+    # Safely get the poster path
+    poster_path = data.get('poster_path')
+
+    if poster_path:
+        # If poster exists, build the full image URL
+        full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+        return full_path
+    else:
+        # Handle missing posters (fallback)
+        return "https://via.placeholder.com/500x750?text=No+Poster+Available"
 
 
 def recommend(movie):
@@ -31,7 +49,7 @@ def recommend(movie):
     recommended_movies = []
     recommended_movie_posters = []
     for i in movies_list:
-        movie_id=i[0]    # for movie id for pposter
+        movie_id=movies.iloc[i[0]].movie_id   # for movie id for pposter
         recommended_movies.append(movies.iloc[i[0]].title)
         #feth poster from api 
         recommended_movie_posters.append(fetch_poster(movie_id))
@@ -49,25 +67,23 @@ st.title("Movie Recommender System")
 selected_movie_name = st.selectbox("Select a movie:", movies["title"].values)
 
 if st.button("Recommend"):
-    recommendations = recommend(selected_movie_name)
+    names,posters = recommend(selected_movie_name)
     st.subheader("Recommended Movies:")
-    for i in recommendations:
-        st.write(i)
-  
-
-col1, col2, col3, col4, col5 = st.columns(5)
-with col1:
-    st.text(recommendations[0][0])
-    st.image(recommendations[1][0])
-with col2:
-    st.text(recommendations[0][1])
-    st.image(recommendations[1][1])
-with col3:
-    st.text(recommendations[0][2])
-    st.image(recommendations[1][2])
-with col4:
-    st.text(recommendations[0][3])
-    st.image(recommendations[1][3])
-with col5:
-    st.text(recommendations[0][4])
-    st.image(recommendations[1][4])
+    #for i in recommendations:
+     #   st.write(i)
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
